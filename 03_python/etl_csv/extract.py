2681efd38c983de949849b690f632737a2d6fba7
akshat-nautiyal07 <akshat.nautiyal@asyvainfotech.com>
2026-04-13 18:58:52 +0530

    Update extract.py

--- 03_python/etl_csv/extract.py
+++ 03_python/etl_csv/extract.py
@@ -3,23 +3,26 @@ Fill in implementation.
 """
 
 import pandas as pd
+import logging
+from google.colab import drive
 
-def transform(customers, orders, products, transactions):
-  customers["email"] = customers["email"].fillna(None)
-  customers["phone"] = customers["phone"].fillna(None)
-  customers["signup_date"] = pd.to_datetime(customers["signup_date"])
+logging.getLogger(__name__)
+drive.mount('/content/drive')
 
-  orders["payment_method"] = orders["payment_method"].fillna("Unknown")
-  orders["order_date"] = pd.to_datetime(orders["order_date"])
-  orders["net_revenue"] = 
 
-  # # Creating a flag for missing ratings
-  # products['rating_missing'] = products['rating'].isna()
-  avg_rating = round(products['rating'].mean(),1)
-  products['rating'] = products['rating'].fillna(avg_rating)
+def extract():
 
-  # Remove any data where price is zero or less than zero
-  before_removing = len(products)
-  products = products[products["price"] > 1]
-  print(f"Product - removed {before_removing - len(products)}")
+  print("------------- EXTRACTING -------------")
+
+  customers = pd.read_csv('/content/drive/MyDrive/Colab Notebooks/etl_customers.csv')
+  products = pd.read_csv('/content/drive/MyDrive/Colab Notebooks/etl_products.csv')
+  orders = pd.read_csv('/content/drive/MyDrive/Colab Notebooks/etl_orders.csv')
+  transactions = pd.read_csv('/content/drive/MyDrive/Colab Notebooks/etl_transactions.csv')
+
+  print(f"Customers: {len(customers)} rows")
+  print(f"Products: {len(products)} rows")
+  print(f"Orders: {len(orders)} rows")
+  print(f"Transactions: {len(transactions)} rows")
+
+  return customers, products, orders, transactions
 
